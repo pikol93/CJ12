b8b0e3d4a2fde0ce12cc1fbe07dfa8d072cb8e05
pikol93 <pikol93@wp.pl>
2023-09-22 22:48:25 +0200

    Added basic rotation

--- Scripts/Character.cs
+++ Scripts/Character.cs
@@ -1,16 +1,43 @@
+using System;
 using Godot;
 
 public partial class Character : CharacterBody3D
 {
 	[Export]
 	private float MovementSpeed { get; set; }
+	[Export]
+	private float MouseSensitivity { get; set; }
+	[Export]
+	private NodePath NeckNodePath { get; set; }
+
+	private Node3D Neck { get; set; }
+	private float Pitch { get; set;}
+
+    public override void _Ready()
+    {
+		Neck = this.GetNodeOrThrow<Node3D>(NeckNodePath);
+    }
 
     public override void _Process(double delta)
-    {
-		Vector2 input = PlayerInputAutoload.Movement;
+	{
+		Vector2 input = PlayerInputAutoload.MoveVector;
 		Vector3 movementDirection = Transform.Basis * new Vector3(input.X, 0, -input.Y).Normalized();
 		Velocity = movementDirection * MovementSpeed;
 		GD.Print($"Input: {input}, movementDirection = {movementDirection}, Velocity = {Velocity}");
 		MoveAndSlide();
-    }
+	}
+
+	public override void _Input(InputEvent inputEvent)
+	{
+		const float halfPi = Mathf.Pi / 2f;
+
+		if (inputEvent is InputEventMouseMotion mouseMotion)
+		{
+			Vector2 mouseMotionVector = mouseMotion.Relative;
+			float pitchDelta = mouseMotionVector.Y * MouseSensitivity;
+			Pitch = Mathf.Clamp(Pitch - pitchDelta, -halfPi, halfPi);
+			RotateY(-mouseMotionVector.X * MouseSensitivity);
+			Neck.Rotation = new Vector3(Pitch, 0f, 0f);
+		}
+	}
 }
--- Scripts/PlayerInputAutoload.cs
+++ Scripts/PlayerInputAutoload.cs
@@ -7,10 +7,12 @@ public partial class PlayerInputAutoload : Node
 	private const string INPUT_NAME_MOVE_LEFT = "move_left";
 	private const string INPUT_NAME_MOVE_RIGHT = "move_right";
 
-    public static Vector2 Movement { get; private set; } = Vector2.Zero;
+    public static Vector2 MoveVector { get; private set; } = Vector2.Zero;
+
+    public static Vector2 MouseMovement { get; private set; } = Vector2.Zero;
 
     public override void _Process(double delta)
     {
-        Movement = Input.GetVector(INPUT_NAME_MOVE_LEFT, INPUT_NAME_MOVE_RIGHT, INPUT_NAME_MOVE_BACKWARD, INPUT_NAME_MOVE_FORWARD).Normalized();
+        MoveVector = Input.GetVector(INPUT_NAME_MOVE_LEFT, INPUT_NAME_MOVE_RIGHT, INPUT_NAME_MOVE_BACKWARD, INPUT_NAME_MOVE_FORWARD).Normalized();
     }
 }
