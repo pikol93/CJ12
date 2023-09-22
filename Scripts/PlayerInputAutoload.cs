9a1994408a1ee0e3d6fefcb20e0fdf0edd6c1831
pikol93 <pikol93@wp.pl>
2023-09-22 23:06:43 +0200

    Added primitive mouse lock toggle

--- Scripts/PlayerInputAutoload.cs
+++ Scripts/PlayerInputAutoload.cs
@@ -6,6 +6,7 @@ public partial class PlayerInputAutoload : Node
 	private const string INPUT_NAME_MOVE_BACKWARD = "move_backward";
 	private const string INPUT_NAME_MOVE_LEFT = "move_left";
 	private const string INPUT_NAME_MOVE_RIGHT = "move_right";
+    private const string INPUT_NAME_TOGGLE_MOUSE_LOCK = "toggle_mouse_lock";
 
     public static Vector2 MoveVector { get; private set; } = Vector2.Zero;
 
@@ -14,5 +15,18 @@ public partial class PlayerInputAutoload : Node
     public override void _Process(double delta)
     {
         MoveVector = Input.GetVector(INPUT_NAME_MOVE_LEFT, INPUT_NAME_MOVE_RIGHT, INPUT_NAME_MOVE_BACKWARD, INPUT_NAME_MOVE_FORWARD).Normalized();
+    
+        if (Input.IsActionJustPressed(INPUT_NAME_TOGGLE_MOUSE_LOCK)) {
+            ToggleMouseLock();
+        }
+    }
+
+    private void ToggleMouseLock()
+    {
+        Input.MouseMode = Input.MouseMode switch
+        {
+            Input.MouseModeEnum.Captured => Input.MouseModeEnum.Visible,
+            _ => Input.MouseModeEnum.Captured,
+        };
     }
 }
